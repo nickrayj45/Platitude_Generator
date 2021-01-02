225feec83cdb1900a14affec5d65097f13f80920
Nicholas Jaquez <nrjaquez@gmail.com>
2021-01-02 14:44:37 -0800

    new platitude added

---
+++ script.js
@@ -0,0 +1,13 @@
+var platitudeList = ['It is what it is', 'The night is always darkest before the dawn', 'Don\'t worry about it', 'Forgive and forget']
+
+var plat = '';
+
+function generatePlat() {
+    for (var i = 0; i <= n; i++) {
+
+    }
+}
+
+console.log(platitudeList)
+
+
