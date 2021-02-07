b0520f51a481c3a0fda6bcbab9e0e63ea31e21a3
Nicholas Jaquez <nrjaquez@gmail.com>
2021-02-06 19:44:45 -0800

    project works

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-var platitudeList = ['It is what it is', 'The night is always darkest before the dawn', 'Don\'t worry about it', 'Forgive and forget']
-
-var plat = '';
-
-function generatePlat() {
-    for (var i = 0; i <= n; i++) {
-
-    }
-}
-
-console.log(platitudeList)
+var platitudeList = ['It is what it is', 'The night is always darkest before the dawn', 'Don\'t worry about it', 'Forgive and forget'];
 
 
+
+
+function generatePlat(platitudeList) {
+    var numberOfPlats = platitudeList.length;
+    var generatedPlat = Math.floor(Math.random() * numberOfPlats);
+    var randomPlat = platitudeList[generatedPlat];
+
+    return randomPlat + " is your platitude.";
+
+
+};
+
+generatePlat(randomPlat);
