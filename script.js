e215ac0273b07158583b2c49104971a8e354a200
Nicholas Jaquez <nrjaquez@gmail.com>
2021-04-19 18:14:02 -0700

    generate function successfully works and platitudes appear on front end

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var platitudeList = ['It is what it is', 'The night is always darkest before the dawn', 'Don\'t worry about it', 'Forgive and forget'];
+var platitudeList = [
+  "It is what it is",
+  "The night is always darkest before the dawn",
+  "Don't worry about it",
+  "Forgive and forget",
+];
 
+function generatePlat() {
+  var numberOfPlats = platitudeList.length;
+  var randomIndex = Math.floor(Math.random() * numberOfPlats);
+  var randomPlat = platitudeList[randomIndex];
 
+  return randomPlat + " is your platitude.";
+}
 
-
-function generatePlat(platitudeList) {
-    var numberOfPlats = platitudeList.length;
-    var generatedPlat = Math.floor(Math.random() * numberOfPlats);
-    var randomPlat = platitudeList[generatedPlat];
-
-    return randomPlat + " is your platitude.";
-
-
-};
-
-generatePlat(randomPlat);
+generatePlat();
+document.getElementById("generate").addEventListener("click", function () {
+  var plat = generatePlat();
+  document.getElementById("platBox").value = plat;
+  console.log(plat);
+});
